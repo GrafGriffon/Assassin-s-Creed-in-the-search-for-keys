351b7d9e754c64b249f771ca29648a2d7d2cd672
GrafGriffon <illiaa553@mail.ru>
2020-10-19 20:12:39 +0300

    fix bug and optimaze

--- ProjectKursach/main.cpp
+++ ProjectKursach/main.cpp
@@ -21,7 +21,7 @@ const int H = 30, W = 50;
 int score = 0, positionJump1 = 0, positionJump2 = 0, counter = 0;
 
 String TileMap[H];
-std::map <char, IntRect> spriteMap = { 
+std::map <char, IntRect> spriteMap = {
 	{'B', IntRect(1, 0, 32, 32)},
 	{'b', IntRect(384, 0, 32, 32)},
 	{'+', IntRect(97, 32, 32, 32)},
@@ -37,7 +37,7 @@ std::map <char, IntRect> spriteMap = {
 	{'D', IntRect(129, 0, 32, 32)},
 	{'d', IntRect(129, 32, 32, 32)},
 	{'Q', IntRect(321, 0, 32, 64)},
-	{'q', IntRect(353, 0, 32, 64)} 
+	{'q', IntRect(353, 0, 32, 64)}
 };
 
 class PLAYER {
@@ -49,16 +49,14 @@ public:
 	bool onGround, isCheckRight = true;
 	Sprite sprite;
 
-	PLAYER(Texture& image)
-	{
+	PLAYER(Texture& image) {
 		sprite.setTexture(image);
 		rect = FloatRect(7 * 32, 22 * 32, 32, 60);
 		dx = dy = 0.1;
 		currentFrame = 0;
 	}
 
-	void update(float time)
-	{
+	void update(float time) {
 		if (time > 90) { time = 90; }
 		rect.left += dx * time;
 		Collision(0);
@@ -69,17 +67,17 @@ public:
 
 		currentFrame += 0.005 * time;
 		if (currentFrame > 5) currentFrame -= 5; // обнуление времени
-		if (currentFrame > 2) {positionHero(currentFrame, 44);}
+		if (currentFrame > 2) { positionHero(currentFrame, 44); }
 		else { positionHero(currentFrame, 41); }
 		if ((!Keyboard::isKeyPressed(Keyboard::Right)) && (!Keyboard::isKeyPressed(Keyboard::Left)) && (!Keyboard::isKeyPressed(Keyboard::Up)))    //стойка персонажа
 		{
-			if (isCheckRight) {sprite.setTextureRect(IntRect(39 * int(currentFrame), 1, 32, 60));}
-			else {sprite.setTextureRect(IntRect(39 * int(currentFrame) + 32, 1, -32, 60));}
+			if (isCheckRight) { sprite.setTextureRect(IntRect(39 * int(currentFrame), 1, 32, 60)); }
+			else { sprite.setTextureRect(IntRect(39 * int(currentFrame) + 32, 1, -32, 60)); }
 		}
-		if (isCheckRight){jump(1);}
-		else {jump(0);}
-		if (Keyboard::isKeyPressed(Keyboard::Left)){isCheckRight = false;}
-		if (Keyboard::isKeyPressed(Keyboard::Right)){isCheckRight = true;}
+		if (isCheckRight) { jump(1); }
+		else { jump(0); }
+		if (Keyboard::isKeyPressed(Keyboard::Left)) { isCheckRight = false; }
+		if (Keyboard::isKeyPressed(Keyboard::Right)) { isCheckRight = true; }
 		sprite.setPosition(rect.left - offsetX, rect.top - offsetY);
 		dx = 0;
 	}
@@ -91,42 +89,39 @@ public:
 
 	void jump(int i) {
 		int x = rect.left / 32;    int y = (rect.top / 32) + 2;
-		if ((TileMap[y][x + i] != 'B') && (TileMap[y][x + i] != 'b')&& (TileMap[y-1][x + i] != 'B') && (TileMap[y-1][x + i] != 'b')&& (TileMap[y-2][x + i] != 'B') && (TileMap[y-2][x + i] != 'b'))
-		{
-			if (isCheckRight) {sprite.setTextureRect(IntRect(335, 273, 32, 60));}
-			else {sprite.setTextureRect(IntRect(367, 273, -32, 60));}
+		if ((TileMap[y][x + i] != 'B') && (TileMap[y][x + i] != 'b') && (TileMap[y - 1][x + i] != 'B') && (TileMap[y - 1][x + i] != 'b') && (TileMap[y - 2][x + i] != 'B') && (TileMap[y - 2][x + i] != 'b')) {
+			if (isCheckRight) { sprite.setTextureRect(IntRect(335, 273, 32, 60)); }
+			else { sprite.setTextureRect(IntRect(367, 273, -32, 60)); }
 		}
 		checkHp(i);
 	}
 
 	void checkHp(int i) {
 		int x, y = (rect.top / 32) + 2;
-		if (isCheckRight) {x = (rect.left - 29) / 32;}
-		else {x = (rect.left + 29) / 32;}
+		if (isCheckRight) { x = (rect.left - 29) / 32; }
+		else { x = (rect.left + 29) / 32; }
 		if ((TileMap[y][x + i] != 'B') && (TileMap[y][x + i] != 'b'))  //право
 		{
-			if (counter == 0){positionJump1 = rect.top;}
+			if (counter == 0) { positionJump1 = rect.top; }
 			positionJump2 = rect.top;
 			counter++;
 		}
 		else {
-			if (positionJump1 != 0)
-			{
+			if (positionJump1 != 0) {
 				int n = positionJump2 - positionJump1;
 				int num = (positionJump2 - positionJump1 + 10) / 32;
-				switch (num)
-				{
-				case 5: {hp -= 20; break;}
-				case 6: {hp -= 28; break;}
+				switch (num) {
+				case 5: {hp -= 20; break; }
+				case 6: {hp -= 28; break; }
 				default: {
 					if (num > 6) {
 						int i = 5 + rand() % 3;
 						hp -= num * i - rand() % 8;
 					}
 					break;
-				}	   
 				}
-				positionJump1=positionJump2=counter= 0;
+				}
+				positionJump1 = positionJump2 = counter = 0;
 			}
 		}
 	}
@@ -134,17 +129,14 @@ public:
 	void Collision(int dir)  //обработка положения игрока
 	{
 		for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
-			for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++)
-			{
-				if ((TileMap[i][j] == 'B') || (TileMap[i][j] == 'b') || (TileMap[i][j] == 'R') || (TileMap[i][j] == 'r') || (TileMap[i][j] == '0'))
-				{
+			for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++) {
+				if ((TileMap[i][j] == 'B') || (TileMap[i][j] == 'b') || (TileMap[i][j] == 'R') || (TileMap[i][j] == 'r') || (TileMap[i][j] == '0')) {
 					if ((dx > 0) && (dir == 0)) rect.left = j * 32 - rect.width;
 					if ((dx < 0) && (dir == 0)) rect.left = j * 32 + 32;
 					if ((dy > 0) && (dir == 1)) { rect.top = i * 32 - rect.height;  dy = 0;   onGround = true; }
 					if ((dy < 0) && (dir == 1)) { rect.top = i * 32 + 32;   dy = 0; }
 				}
-				if (TileMap[i][j] == 'K')
-				{
+				if (TileMap[i][j] == 'K') {
 					TileMap[i][j] = '+';
 					score++;
 				}
@@ -159,11 +151,11 @@ void printInfo(RenderWindow& window) {
 	Text textHp("", font, 35);
 	textHp.setStyle(Text::Bold);
 	if (hp <= 100 && hp > 80) { textHp.setFillColor(sf::Color(255, 255, 0)); }
-	if (hp <= 80 && hp > 60) {textHp.setFillColor(sf::Color(255, 215, 0));}
-	if (hp <= 60 && hp > 45) { textHp.setFillColor(sf::Color(255, 265, 0));}
-	if (hp <= 45 && hp > 30) { textHp.setFillColor(sf::Color(255, 140, 0));}
-	if (hp <= 30 && hp > 15) { textHp.setFillColor(sf::Color(255, 69, 0));}
-	if (hp <= 15) { textHp.setFillColor(sf::Color(255, 0, 0));}
+	if (hp <= 80 && hp > 60) { textHp.setFillColor(sf::Color(255, 215, 0)); }
+	if (hp <= 60 && hp > 45) { textHp.setFillColor(sf::Color(255, 265, 0)); }
+	if (hp <= 45 && hp > 30) { textHp.setFillColor(sf::Color(255, 140, 0)); }
+	if (hp <= 30 && hp > 15) { textHp.setFillColor(sf::Color(255, 69, 0)); }
+	if (hp <= 15) { textHp.setFillColor(sf::Color(255, 0, 0)); }
 	std::ostringstream scorePlayer, hpPlayer;
 	scorePlayer << score;
 	hpPlayer << hp;
@@ -186,27 +178,23 @@ char positionKey(int& keys, int& noKey, char TileMap) {
 				noKey++;
 			}
 		}
+		printf("%c\n", TileMap);
 		keys--;
 	}
 	return TileMap;
 }
 
-void printMap(RenderWindow& window, Sprite sp1, int& noKey, int& keys, PLAYER p) {
-	int startHorisontal = (p.rect.left / 32)-13;
+void printMap(RenderWindow& window, Sprite sp1, PLAYER p) {
+	int startHorisontal = (p.rect.left / 32) - 13;
 	int stopHorisontal = startHorisontal + 2 * 13;
-	if (startHorisontal < 0) {startHorisontal = 0;}
+	if (startHorisontal < 0) { startHorisontal = 0; }
 	if (startHorisontal >= W - 2 * 13) { startHorisontal = W - 2 * 13; stopHorisontal = W; }
-	int startVertical = (p.rect.top / 32)-8;
-	if (startVertical < 9) { startVertical = 0;}
-	int stopVeertical = (p.rect.top / 32)+9;
+	int startVertical = (p.rect.top / 32) - 8;
+	if (startVertical < 9) { startVertical = 0; }
+	int stopVeertical = (p.rect.top / 32) + 9;
 	if (stopVeertical > H) { stopVeertical = H; };
 	for (int i = startVertical; i < stopVeertical; i++) {
-		for (int j = startHorisontal; j < W; j++)
-		{
-			if (TileMap[i][j] == 'K')
-			{
-				TileMap[i][j]=positionKey(keys, noKey, TileMap[i][j]);
-			}
+		for (int j = startHorisontal; j < W; j++) {
 			if (TileMap[i][j] == ' ') { continue; }
 			else { sp1.setTextureRect(spriteMap[TileMap[i][j]]); }
 			sp1.setPosition(j * 32 - offsetX, i * 32 - offsetY);
@@ -231,8 +219,7 @@ void viewInfo(Sprite sprite) {
 
 void eventWindow() {
 	Event event;
-	while (window.pollEvent(event))
-	{
+	while (window.pollEvent(event)) {
 		if (event.type == Event::Closed)
 			window.close();
 	}
@@ -244,8 +231,7 @@ void printResult() {
 	text.setStyle(Text::Bold);
 	std::ostringstream scorePlayer;
 	scorePlayer << score;
-	if (hp < 1)
-	{
+	if (hp < 1) {
 		window.clear(Color::Red);
 		text.setString("  YOU LOSE\n Score: " + scorePlayer.str() + "/3");
 	}
@@ -259,8 +245,7 @@ void printResult() {
 
 void returnKeys() {
 	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++)
-		{
+		for (int j = 0; j < W; j++) {
 			if (TileMap[i][j] != '+') {
 				continue;
 			}
@@ -273,8 +258,7 @@ int printPrewiev(Sprite sprite, int menu, bool checkMusic) {
 	window.clear(Color::Black);
 	window.draw(sprite);
 	window.display();
-	if (!checkMusic)
-	{
+	if (!checkMusic) {
 		if (IntRect(0, 0, 1920, 1080).contains(Mouse::getPosition(window))) { menu = 0; }
 		if (!Mouse::isButtonPressed(Mouse::Left)) { menu = -1; }
 	}
@@ -288,47 +272,42 @@ void printMenu(RectangleShape& choice, Sprite sprite1, Sprite sprite2, Clock& ti
 	window.draw(sprite1);
 	window.draw(sprite2);
 	window.display();
-;	if (time.getElapsedTime().asSeconds() > 0.3)
-	{
-		if (Keyboard::isKeyPressed(Keyboard::Up))
-		{
+	;	if (time.getElapsedTime().asSeconds() > 0.3) {
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
 			enterChoice--;
-			if (enterChoice < 1){enterChoice = 3;}
+			if (enterChoice < 1) { enterChoice = 3; }
 			time.restart();
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Down))
-		{
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
 			enterChoice++;
-			if (enterChoice > 3){enterChoice = 1;}
+			if (enterChoice > 3) { enterChoice = 1; }
 			time.restart();
 		}
 	}
-	switch (enterChoice)
-	{
-	case 1: {choice.setPosition(450, 149); break;}
-	case 2: {choice.setPosition(450, 208); break;}
-	case 3: {choice.setPosition(450, 272); break;}
+	switch (enterChoice) {
+	case 1: {choice.setPosition(450, 149); break; }
+	case 2: {choice.setPosition(450, 208); break; }
+	case 3: {choice.setPosition(450, 272); break; }
 	}
 }
-	
+
 void gameplay(Sprite sprite1, Sprite sprite2, Texture t) {
-	if ((int)rand()%2) {
+	if ((int)rand() % 2) {
 		sprite1.setTextureRect(IntRect(0, 1500, 800, 500));
-	} else { sprite1.setTextureRect(IntRect(0, 1000, 800, 500)); }
+	}
+	else { sprite1.setTextureRect(IntRect(0, 1000, 800, 500)); }
 	PLAYER p(t);
 	Clock clock;
 	SoundBuffer buffer;
 	buffer.loadFromFile("Audio\\jump.wav");
 	Sound sound;
 	sound.setBuffer(buffer);
-	int noKey = 0, keys = 5;
 	hp = 100;
 	score = 0;
 	int x, y;
 	offsetX = p.rect.left - 224;
-	while (hp > 0 && score < 3)
-	{
+	while (hp > 0 && score < 3) {
 		float time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
 
@@ -354,21 +333,27 @@ void gameplay(Sprite sprite1, Sprite sprite2, Texture t) {
 		offsetY = p.rect.top - 250;
 		window.clear(Color::Black);
 		window.draw(sprite1);   ///
-		printMap(window, sprite2, noKey, keys, p);
+		printMap(window, sprite2, p);
 		printInfo(window);
 		window.draw(p.sprite);
 		window.display();
-		while (Keyboard::isKeyPressed(Keyboard::Escape)){ hp = 0; }
+		while (Keyboard::isKeyPressed(Keyboard::Escape)) { hp = 0; }
 	}
 }
 
 void readMap(const char name[]) {
 	using namespace std;
 	int num = 0;
+	int noKey = 0, keys = 5;
 	string s;
 	ifstream file1(name);
 	while (getline(file1, s)) {
 		TileMap[num] = s;
+		for (int i = 0; i < W; i++) {
+			if (TileMap[num][i] == 'K') {
+				TileMap[num][i] = positionKey(keys, noKey, TileMap[num][i]);
+			}
+		}
 		num++;
 	}
 	file1.close();
@@ -399,14 +384,14 @@ void setSprite(Sprite& sprite0, Sprite& sprite1, Sprite& sprite2, Sprite& sprite
 }
 
 void setMap(RectangleShape& choice, Sprite sprite, Clock& time, int& menu) {
-	int enterChoice =1;
+	int enterChoice = 1;
 	int numMap;
 	while (1) {
 		window.clear(Color::Black);
 		window.draw(sprite);
 		window.draw(choice);
 		window.display();
-		if (Keyboard::isKeyPressed(Keyboard::Enter)  && time.getElapsedTime().asSeconds() > 0.3) { break; }
+		if (Keyboard::isKeyPressed(Keyboard::Enter) && time.getElapsedTime().asSeconds() > 0.3) { break; }
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) { menu = 0; return; }
 		if (time.getElapsedTime().asSeconds() > 0.3) {
 			if (Keyboard::isKeyPressed(Keyboard::Left)) {
@@ -417,18 +402,18 @@ void setMap(RectangleShape& choice, Sprite sprite, Clock& time, int& menu) {
 
 			if (Keyboard::isKeyPressed(Keyboard::Up)) {
 				enterChoice--;
-				if ((enterChoice < 1)||(enterChoice < 11 && enterChoice>9)) { enterChoice += 2; }
+				if ((enterChoice < 1) || (enterChoice < 11 && enterChoice>9)) { enterChoice += 2; }
 				time.restart();
 			}
 
 			if (Keyboard::isKeyPressed(Keyboard::Down)) {
-				enterChoice+=1;
-				if ((enterChoice > 2 && enterChoice < 10)||(enterChoice > 12)) { enterChoice -=2; }
+				enterChoice += 1;
+				if ((enterChoice > 2 && enterChoice < 10) || (enterChoice > 12)) { enterChoice -= 2; }
 				time.restart();
 			}
 
 			if (Keyboard::isKeyPressed(Keyboard::Right)) {
-				enterChoice+=10;
+				enterChoice += 10;
 				if (enterChoice > 20) { enterChoice -= 20; }
 				time.restart();
 			}
@@ -450,8 +435,7 @@ void setMap(RectangleShape& choice, Sprite sprite, Clock& time, int& menu) {
 	}
 }
 
-int main()
-{
+int main() {
 	time_t seconds;
 	seconds = time(NULL);
 	srand(seconds);
@@ -476,31 +460,27 @@ int main()
 	setSprite(sprite[0], sprite[1], sprite[2], sprite[3], sprite[4], sprite[5], sprite[6], tx, pr);
 	Clock time;
 
-	while (menu != 3)
-	{
-		if (menu == -1){menu = printPrewiev(sprite[5], menu, soundPrev.getStatus());}
-		if (menu == 0)
-		{
+	while (menu != 3) {
+		if (menu == -1) { menu = printPrewiev(sprite[5], menu, soundPrev.getStatus()); }
+		if (menu == 0) {
 			musicGame.stop();
 			musicMenu.setLoop(true);
-			if (!musicMenu.getStatus()) { musicMenu.play();}
+			if (!musicMenu.getStatus()) { musicMenu.play(); }
 			printMenu(choice, sprite[2], sprite[3], time, enterChoice);
-			
+
 			if (Keyboard::isKeyPressed(Keyboard::Enter)) { menu = enterChoice; time.restart(); }
 			eventWindow();
 		}
 
-		if (menu == 2)
-		{
+		if (menu == 2) {
 			viewInfo(sprite[4]);
-			if ((Keyboard::isKeyPressed(Keyboard::Escape))){menu = 0;}
+			if ((Keyboard::isKeyPressed(Keyboard::Escape))) { menu = 0; }
 			eventWindow();
 		}
 
-		if (menu == 1)
-		{
+		if (menu == 1) {
 			setMap(choiceMap, sprite[6], time, menu);
-			if (menu!=0) {
+			if (menu != 0) {
 				musicMenu.stop();
 				musicGame.setLoop(true);
 				if (!musicGame.getStatus()) { musicGame.play(); }
